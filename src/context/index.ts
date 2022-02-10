cfefc98d4fdf55df883da3cd487fa66196982295
armankuanysh <armank@rocketfirm.com>
2022-02-11 01:08:43 +0600

    chore(context): update import paths

--- src/context/index.ts
+++ src/context/index.ts
@@ -2,17 +2,17 @@
 import Chalk from 'chalk'
 import Yargs from 'yargs'
 /** modules */
-import Status from 'modules/status.js'
+import Status from 'modules/status/index.js'
 import Components from 'modules/components/index.js'
 /** services */
 import Commands from 'services/commands.js'
 import Shell from 'services/shell.js'
 /** utils */
-import Logs from 'utils/logs.js'
+import Alerts from 'utils/alerts.js'
 
 const chalk = Chalk
 
-const logs = new Logs(chalk)
+const logs = new Alerts(chalk)
 const status = new Status(chalk, logs)
 const components = new Components(logs)
 const commands = new Commands(status, components)
